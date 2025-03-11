f6d886144cf04ec7ca53ac40f1dd2a98408e9f20
Jacky <jackyllk1116@gmail.com>
2025-03-11 20:57:56 +0800

    Create Homepage and Login page

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-export default function App() {
+const App = () => {
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    navigation.navigate("Home");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Login Form</Text>
+      <TextInput style={styles.input} placeholder="Email" />
+      <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 24,
+    fontWeight: "bold",
+    color: "blue",
+  },
+  input: {
+    width: "80%",
+    padding: 8,
+    marginBottom: 8,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 4,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
   },
 });
+
+export default App;
